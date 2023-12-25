975e5cb1bc99548987c3619e0eac188171aea389
PedroSilvDev-0 <silvsrhyan@gmail.com>
2023-12-25 11:26:25 -0300

    Inicio-Navbar

---
+++ src/componentes/Navbar.js
@@ -0,0 +1,13 @@
+function Navbar() {
+    return(
+        <div>
+            <ul className='text-2-1 font-bold flex flex justify-end bg-blue-500'>
+                <li className="bg-blue-500 hover:bg-blue-600 text-white flex items-center justify-center w-32 h-10"> Home1 </li>
+                <li className="bg-blue-500 hover:bg-blue-600 text-white flex items-center justify-center w-32 h-10"> Home2 </li>
+                <li className="bg-blue-500 hover:bg-blue-600 text-white flex items-center justify-center w-32 h-10"> Home3 </li>
+            </ul>
+        </div>
+    )
+}
+
+export default Navbar
